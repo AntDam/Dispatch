88f2b4282fea31eb21c89e79aac1d05b91b783eb
Ant <Akroma17@hotmail.com>
2025-05-28 11:16:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-
+<script>
+    alert("This is a page to showcase what I have performed in the past few years.  This will also serve as a display of what I've learned and taught myself with coding.");")
+</script>
